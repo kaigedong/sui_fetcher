2db07304e9a7bac2719cea5745adcd6beecf87c3
kaigedong <dongkaige@gmail.com>
2025-08-08 22:58:58 +0800

    refa

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -1,204 +1,207 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Result, bail};
+use bigdecimal::BigDecimal;
+use futures::{future, stream::StreamExt};
 use mini_macro::here;
-use sui_sdk::rpc_types::{
-    SuiTransactionBlockResponseOptions, SuiTransactionBlockResponseQuery, TransactionFilter,
+use std::str::FromStr;
+use sui_sdk::{
+    SuiClient, SuiClientBuilder,
+    rpc_types::{
+        SuiTransactionBlockEffects, SuiTransactionBlockEffectsAPI, SuiTransactionBlockResponse,
+        SuiTransactionBlockResponseOptions, SuiTransactionBlockResponseQuery, TransactionFilter,
+    },
 };
+use sui_types::base_types::SuiAddress;
 
-use super::transfer;
-use crate::Fetcher;
+use crate::{Dex, Swap, TxType, errors::DecodeError, transfer};
 
-pub async fn get_latest_txs(fetcher: &Fetcher, filter: TransactionFilter) -> Result<()> {
-    let options = SuiTransactionBlockResponseOptions::new()
-        .with_events()
-        .with_balance_changes()
-        .with_effects();
-    let query = SuiTransactionBlockResponseQuery::new(Some(filter), Some(options));
+pub struct Fetcher {
+    sui_client: SuiClient,
+    who: SuiAddress,
+    old_first: bool,
+}
 
-    let txs = fetcher
-        .sui_client
-        .read_api()
-        .query_transaction_blocks(query.clone(), None, None, true)
-        .await
-        .context(here!())?;
+impl Fetcher {
+    pub async fn new_mainnet(who: &str, old_first: bool) -> Result<Self> {
+        let sui_client = SuiClientBuilder::default().build_mainnet().await?;
 
-    // let mut out = vec![];
+        Ok(Self {
+            sui_client,
+            who: SuiAddress::from_str(who).context(here!())?,
+            old_first,
+        })
+    }
 
-    for tx in txs.data {
-        if let Some(status) = tx.status_ok() {
-            if !status {
-                continue;
+    pub async fn fetch_txs(self, by_from: bool) -> Result<()> {
+        let filter = if by_from {
+            TransactionFilter::FromAddress(self.who)
+        } else {
+            TransactionFilter::ToAddress(self.who)
+        };
+
+        let options = SuiTransactionBlockResponseOptions::default()
+            .with_effects()
+            .with_events()
+            .with_balance_changes();
+        let filter = SuiTransactionBlockResponseQuery::new(Some(filter), Some(options));
+
+        let descending_order = if self.old_first { false } else { true };
+        let txs =
+            self.sui_client
+                .read_api()
+                .get_transactions_stream(filter, None, descending_order);
+
+        txs.for_each(|tx_resp| {
+            // println!("{:?}", tx_resp);
+            Self::log_sui_tx_resp(tx_resp);
+            future::ready(())
+        })
+        .await;
+
+        // for event in events.pull_next() {}
+        Ok(())
+    }
+
+    fn is_err(tx_resp: &SuiTransactionBlockResponse) -> Result<bool> {
+        let res = tx_resp
+            .effects
+            .as_ref()
+            .map(|e| e.status().is_err())
+            .ok_or(DecodeError::TransactionResponseWithoutEffects)?;
+        Ok(res)
+    }
+
+    fn log_sui_tx_resp(tx_resp: SuiTransactionBlockResponse) {
+        if Self::is_err(&tx_resp).unwrap() {
+            return;
+        }
+
+        Self::decode_tx_type(tx_resp.clone()).unwrap();
+    }
+
+    // TODO: 允许用户自己注册解码代码！
+    fn decode_tx_type(tx_resp: SuiTransactionBlockResponse) -> Result<TxType> {
+        tracing::info!("{}", tx_resp.timestamp_ms.unwrap());
+
+        let events = tx_resp.events.as_ref().unwrap();
+        if events.data.is_empty() {
+            let balance_changes = tx_resp.balance_changes.unwrap();
+            let transfer_event = transfer::decode_transfer(balance_changes).context(here!())?;
+
+            println!("#### transfer...");
+
+            if transfer_event.sender.eq(&transfer_event.receiver) {
+                return Ok(TxType::SelfTransfer(transfer_event));
+            }
+            return Ok(TxType::Transfer(transfer_event));
+        }
+
+        for event in &events.data {
+            if event.type_.to_string()
+                == "0x1eabed72c53feb3805120a081dc15963c204dc8d091542592abaf7a35689b2fb::pool::SwapEvent"
+            {
+                println!("CetusSwap");
+
+                let balance_changes = tx_resp.balance_changes.as_ref().unwrap();
+
+                assert!(
+                    balance_changes.len() == 2,
+                    "{}",
+                    serde_json::to_string(&tx_resp).unwrap()
+                );
+                let asset1_amount = balance_changes[0].amount;
+                let asset2_amount = balance_changes[1].amount;
+                let (in_token, out_token) = if asset1_amount > asset2_amount {
+                    (
+                        balance_changes[1].coin_type.to_string(),
+                        balance_changes[0].coin_type.to_string(),
+                    )
+                } else {
+                    (
+                        balance_changes[0].coin_type.to_string(),
+                        balance_changes[1].coin_type.to_string(),
+                    )
+                };
+
+                let _e = &event.parsed_json;
+                return Ok(TxType::Swap(Swap {
+                    pool: _e.get("pool").unwrap().as_str().unwrap().to_string(),
+                    dex: Dex::Cetus,
+                    a2b: event.parsed_json.get("atob").unwrap().as_bool().unwrap(),
+                    in_amount: _e
+                        .get("amount_in")
+                        .unwrap()
+                        .as_str()
+                        .unwrap()
+                        .parse::<i128>()
+                        .unwrap(),
+                    out_amount: _e
+                        .get("amount_out")
+                        .unwrap()
+                        .as_str()
+                        .unwrap()
+                        .parse::<i128>()?,
+                    in_token,
+                    out_token,
+                }));
+            } else if event.type_.to_string()
+                == "0x3492c874c1e3b3e2984e8c41b589e642d4d0a5d6459e5a9cfc2d52fd7c89c267::events::AssetSwap"
+            {
+                println!("BluefinSwap");
+
+                let balance_changes = tx_resp.balance_changes.as_ref().unwrap();
+                assert!(
+                    balance_changes.len() == 2,
+                    "{}",
+                    serde_json::to_string(&tx_resp).unwrap()
+                );
+                let asset1_amount = balance_changes[0].amount;
+                let asset2_amount = balance_changes[1].amount;
+                let (in_token, out_token) = if asset1_amount > asset2_amount {
+                    (
+                        balance_changes[1].coin_type.to_string(),
+                        balance_changes[0].coin_type.to_string(),
+                    )
+                } else {
+                    (
+                        balance_changes[0].coin_type.to_string(),
+                        balance_changes[1].coin_type.to_string(),
+                    )
+                };
+
+                let _e = &event.parsed_json;
+                return Ok(TxType::Swap(Swap {
+                    pool: _e.get("pool_id").unwrap().as_str().unwrap().to_string(),
+                    dex: Dex::Bluefin,
+                    a2b: event.parsed_json.get("a2b").unwrap().as_bool().unwrap(),
+                    in_amount: _e
+                        .get("amount_in")
+                        .unwrap()
+                        .as_str()
+                        .unwrap()
+                        .parse::<i128>()
+                        .unwrap(),
+                    out_amount: _e
+                        .get("amount_out")
+                        .unwrap()
+                        .as_str()
+                        .unwrap()
+                        .parse::<i128>()?,
+                    in_token,
+                    out_token,
+                }));
+            } else {
+                println!("### txs: {}", serde_json::to_string(&tx_resp).unwrap());
             }
         }
 
-        let Some(events) = tx.events else {
-            unreachable!()
-        };
+        println!("### txs: {}", serde_json::to_string(&tx_resp).unwrap());
 
-        if events.data.is_empty() {
-            if let Some(balance_change) = tx.balance_changes {
-                if balance_change.len() != 2 && balance_change.len() != 3 {
-                    tracing::info!(
-                        "### unknown tx type: {}, {}",
-                        tx.digest,
-                        balance_change.len()
-                    );
-                }
-                let transfer_event = transfer::decode_transfer(balance_change).unwrap();
-
-                // let warp = WarpEvents::new(
-                //     transfer_event,
-                //     tx.timestamp_ms.unwrap_or_default(),
-                //     tx.digest,
-                //     "Transer",
-                // )
-                // .await;
-                // out.push(MonitorEvent::Transfer(warp));
-            };
-        }
-
-        for event in events.data {
-            // match event.type_.to_string().as_ref() {
-            //     "0x4a35d3dfef55ed3631b7158544c6322a23bc434fe4fca1234cb680ce0505f82d::pool::CollectRewardEvent" =>
-            //     {
-            //         let event: CollectRewardEvent =
-            //             serde_json::from_value(event.parsed_json).unwrap();
-
-            //         let warp = WarpEvents::new(
-            //             event,
-            //             tx.timestamp_ms.unwrap_or_default(),
-            //             tx.digest,
-            //             "CollectRewardEvent",
-            //         )
-            //         .await;
-            //         out.push(MonitorEvent::CollectReward(warp));
-            //     }
-            //     "0x4a35d3dfef55ed3631b7158544c6322a23bc434fe4fca1234cb680ce0505f82d::pool::RemoveLiquidityEvent" =>
-            //     {
-            //         let event: RemoveLiquidityEvent =
-            //             serde_json::from_value(event.parsed_json).unwrap();
-            //         let warp = WarpEvents::new(
-            //             event,
-            //             tx.timestamp_ms.unwrap_or_default(),
-            //             tx.digest,
-            //             "RemoveLiquidityEvent",
-            //         )
-            //         .await;
-            //         out.push(MonitorEvent::RemoveLiquidity(warp));
-            //     }
-            //     "0x4a35d3dfef55ed3631b7158544c6322a23bc434fe4fca1234cb680ce0505f82d::pool::CollectFeeEvent" =>
-            //     {
-            //         let event: CollectFeeEvent = serde_json::from_value(event.parsed_json).unwrap();
-            //         let warp = WarpEvents::new(
-            //             event,
-            //             tx.timestamp_ms.unwrap_or_default(),
-            //             tx.digest,
-            //             "CollectFeeEvent",
-            //         )
-            //         .await;
-            //         out.push(MonitorEvent::CollectFee(warp));
-            //     }
-            //     "0x4a35d3dfef55ed3631b7158544c6322a23bc434fe4fca1234cb680ce0505f82d::pool::ClosePositionEvent" =>
-            //     {
-            //         let event: ClosePositionEvent =
-            //             serde_json::from_value(event.parsed_json).unwrap();
-            //         let warp = WarpEvents::new(
-            //             event,
-            //             tx.timestamp_ms.unwrap_or_default(),
-            //             tx.digest,
-            //             "ClosePositionEvent",
-            //         )
-            //         .await;
-            //         out.push(MonitorEvent::ClosePosition(warp));
-            //         // log_event(&serde_json::to_string(&warp).unwrap(), LOG_FILE_NAME).unwrap();
-            //     }
-            //     "0x4a35d3dfef55ed3631b7158544c6322a23bc434fe4fca1234cb680ce0505f82d::pool::OpenPositionEvent" =>
-            //     {
-            //         let event: OpenPositionEvent =
-            //             serde_json::from_value(event.parsed_json).unwrap();
-            //         let warp = WarpEvents::new(
-            //             event,
-            //             tx.timestamp_ms.unwrap_or_default(),
-            //             tx.digest,
-            //             "OpenPositionEvent",
-            //         )
-            //         .await;
-            //         out.push(MonitorEvent::OpenPosition(warp));
-            //     }
-            //     "0x4a35d3dfef55ed3631b7158544c6322a23bc434fe4fca1234cb680ce0505f82d::pool::AddLiquidityEvent" =>
-            //     {
-            //         let event: AddLiquidityEvent =
-            //             serde_json::from_value(event.parsed_json).unwrap();
-            //         let warp = WarpEvents::new(
-            //             event,
-            //             tx.timestamp_ms.unwrap_or_default(),
-            //             tx.digest,
-            //             "AddLiquidityEvent",
-            //         )
-            //         .await;
-            //         out.push(MonitorEvent::AddLiquidity(warp));
-            //     }
-            //     "0x4a35d3dfef55ed3631b7158544c6322a23bc434fe4fca1234cb680ce0505f82d::pool::SwapEvent" =>
-            //     {
-            //         let event: SwapEvent = serde_json::from_value(event.parsed_json).unwrap();
-            //         let warp = WarpEvents::new(
-            //             event,
-            //             tx.timestamp_ms.unwrap_or_default(),
-            //             tx.digest,
-            //             "SwapEvent",
-            //         )
-            //         .await;
-            //         out.push(MonitorEvent::Swap(warp));
-            //     }
-            //     "0x4a35d3dfef55ed3631b7158544c6322a23bc434fe4fca1234cb680ce0505f82d::partner::ReceiveRefFeeEvent" =>
-            //     {
-            //         let event: ReceiveRefFeeEvent =
-            //             serde_json::from_value(event.parsed_json).unwrap();
-            //         let warp = WarpEvents::new(
-            //             event,
-            //             tx.timestamp_ms.unwrap_or_default(),
-            //             tx.digest,
-            //             "ReceiveRefFeeEvent",
-            //         )
-            //         .await;
-            //         out.push(MonitorEvent::ReceiveRefFee(warp));
-            //     }
-            //     "0x70285592c97965e811e0c6f98dccc3a9c2b4ad854b3594faab9597ada267b860::liquidity::OpenPositionEvent" =>
-            //     {
-            //         let _event: MMTOpenPositionEvent =
-            //             serde_json::from_value(event.parsed_json).unwrap();
-            //     }
-            //     "0x70285592c97965e811e0c6f98dccc3a9c2b4ad854b3594faab9597ada267b860::liquidity::AddLiquidityEvent" =>
-            //     {
-            //         let _event: MMTAddLiquidityEvent =
-            //             serde_json::from_value(event.parsed_json).unwrap();
-            //     }
-            //     "0x70285592c97965e811e0c6f98dccc3a9c2b4ad854b3594faab9597ada267b860::liquidity::RemoveLiquidityEvent" =>
-            //     {
-            //         let _event: MMTRemoveLiquidityEvent =
-            //             serde_json::from_value(event.parsed_json).unwrap();
-            //     }
-            //     "0x70285592c97965e811e0c6f98dccc3a9c2b4ad854b3594faab9597ada267b860::collect::CollectPoolRewardEvent" =>
-            //     {
-            //         let _event: MMTCollectPoolRewardEvent =
-            //             serde_json::from_value(event.parsed_json).unwrap();
-            //     }
-            //     "0x70285592c97965e811e0c6f98dccc3a9c2b4ad854b3594faab9597ada267b860::collect::FeeCollectedEvent" =>
-            //     {
-            //         let _event: MMTFeeCollectedEvent =
-            //             serde_json::from_value(event.parsed_json).unwrap();
-            //     }
-
-            //     "0x70285592c97965e811e0c6f98dccc3a9c2b4ad854b3594faab9597ada267b860::liquidity::ClosePositionEvent" =>
-            //     {
-            //         let _event: MMTClosePositionEvent =
-            //             serde_json::from_value(event.parsed_json).unwrap();
-            //     }
-            //     _ => {
-            //         unreachable!("{}, {:?}", event.type_.to_string(), event.id)
-            //     }
-            // }
-        }
+        bail!("Unknown tx type")
     }
-    tokio::time::sleep(std::time::Duration::from_millis(200)).await;
-    Ok(())
+
+    pub fn tx_gas(effect: SuiTransactionBlockEffects) -> BigDecimal {
+        let fee = effect.gas_cost_summary();
+        let fee = fee.computation_cost + fee.storage_cost - fee.storage_rebate;
+        BigDecimal::from(fee) / 10u128.pow(9)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,221 +2,7 @@ pub mod errors;
 pub mod fetcher;
 pub mod transfer;
 
-use anyhow::{Context, Result, bail};
-use bigdecimal::BigDecimal;
-use futures::{future, stream::StreamExt};
-use mini_macro::here;
-use std::str::FromStr;
-use sui_sdk::{
-    SuiClient, SuiClientBuilder,
-    rpc_types::{
-        SuiTransactionBlockEffects, SuiTransactionBlockEffectsAPI, SuiTransactionBlockResponse,
-        SuiTransactionBlockResponseOptions, SuiTransactionBlockResponseQuery, TransactionFilter,
-    },
-};
-use sui_types::base_types::SuiAddress;
-
-use crate::{errors::DecodeError, transfer::TransferEvent};
-
-pub struct Fetcher {
-    sui_client: SuiClient,
-    who: SuiAddress,
-    old_first: bool,
-}
-
-impl Fetcher {
-    pub async fn new_mainnet(who: &str, old_first: bool) -> Result<Self> {
-        let sui_client = SuiClientBuilder::default().build_mainnet().await?;
-
-        Ok(Self {
-            sui_client,
-            who: SuiAddress::from_str(who).context(here!())?,
-            old_first,
-        })
-    }
-
-    pub async fn fetch_txs(self) -> Result<()> {
-        let tx_filter_from = TransactionFilter::FromAddress(self.who);
-        let tx_filter_to = TransactionFilter::ToAddress(self.who);
-
-        let options = SuiTransactionBlockResponseOptions::default()
-            .with_effects()
-            .with_events()
-            .with_balance_changes();
-        let filter = SuiTransactionBlockResponseQuery::new(Some(tx_filter_from), Some(options));
-
-        let descending_order = if self.old_first { false } else { true };
-        let txs =
-            self.sui_client
-                .read_api()
-                .get_transactions_stream(filter, None, descending_order);
-
-        txs.for_each(|tx_resp| {
-            // println!("{:?}", tx_resp);
-            Self::log_sui_tx_resp(tx_resp);
-            future::ready(())
-        })
-        .await;
-
-        // for event in events.pull_next() {}
-        Ok(())
-    }
-
-    fn is_err(tx_resp: &SuiTransactionBlockResponse) -> Result<bool> {
-        let res = tx_resp
-            .effects
-            .as_ref()
-            .map(|e| e.status().is_err())
-            .ok_or(DecodeError::TransactionResponseWithoutEffects)?;
-        Ok(res)
-    }
-
-    fn log_sui_tx_resp(tx_resp: SuiTransactionBlockResponse) {
-        if Self::is_err(&tx_resp).unwrap() {
-            return;
-        }
-
-        Self::decode_tx_type(tx_resp.clone()).unwrap();
-
-        // let events = tx_resp.events.as_ref().unwrap();
-        // if events.data.is_empty() {
-        //     // 这是transfer交易!
-        //     println!("{}", serde_json::to_string(&tx_resp).unwrap());
-        // } else {
-        //     // 这是dex交易！
-        //     todo!()
-        // }
-
-        // let balance_changes = tx_resp.balance_changes.unwrap();
-    }
-
-    // TODO: 允许用户自己注册解码代码！
-    fn decode_tx_type(tx_resp: SuiTransactionBlockResponse) -> Result<TxType> {
-        tracing::info!("{}", tx_resp.timestamp_ms.unwrap());
-
-        let events = tx_resp.events.as_ref().unwrap();
-        if events.data.is_empty() {
-            let balance_changes = tx_resp.balance_changes.unwrap();
-            let transfer_event = transfer::decode_transfer(balance_changes).context(here!())?;
-
-            println!("#### transfer...");
-
-            if transfer_event.sender.eq(&transfer_event.receiver) {
-                return Ok(TxType::SelfTransfer(transfer_event));
-            }
-            return Ok(TxType::Transfer(transfer_event));
-        }
-
-        for event in &events.data {
-            if event.type_.to_string()
-                == "0x1eabed72c53feb3805120a081dc15963c204dc8d091542592abaf7a35689b2fb::pool::SwapEvent"
-            {
-                println!("CetusSwap");
-
-                let balance_changes = tx_resp.balance_changes.as_ref().unwrap();
-
-                assert!(
-                    balance_changes.len() == 2,
-                    "{}",
-                    serde_json::to_string(&tx_resp).unwrap()
-                );
-                let asset1_amount = balance_changes[0].amount;
-                let asset2_amount = balance_changes[1].amount;
-                let (in_token, out_token) = if asset1_amount > asset2_amount {
-                    (
-                        balance_changes[1].coin_type.to_string(),
-                        balance_changes[0].coin_type.to_string(),
-                    )
-                } else {
-                    (
-                        balance_changes[0].coin_type.to_string(),
-                        balance_changes[1].coin_type.to_string(),
-                    )
-                };
-
-                let _e = &event.parsed_json;
-                return Ok(TxType::Swap(Swap {
-                    pool: _e.get("pool").unwrap().as_str().unwrap().to_string(),
-                    dex: Dex::Cetus,
-                    a2b: event.parsed_json.get("atob").unwrap().as_bool().unwrap(),
-                    in_amount: _e
-                        .get("amount_in")
-                        .unwrap()
-                        .as_str()
-                        .unwrap()
-                        .parse::<i128>()
-                        .unwrap(),
-                    out_amount: _e
-                        .get("amount_out")
-                        .unwrap()
-                        .as_str()
-                        .unwrap()
-                        .parse::<i128>()?,
-                    in_token,
-                    out_token,
-                }));
-            } else if event.type_.to_string()
-                == "0x3492c874c1e3b3e2984e8c41b589e642d4d0a5d6459e5a9cfc2d52fd7c89c267::events::AssetSwap"
-            {
-                println!("BluefinSwap");
-
-                let balance_changes = tx_resp.balance_changes.as_ref().unwrap();
-                assert!(
-                    balance_changes.len() == 2,
-                    "{}",
-                    serde_json::to_string(&tx_resp).unwrap()
-                );
-                let asset1_amount = balance_changes[0].amount;
-                let asset2_amount = balance_changes[1].amount;
-                let (in_token, out_token) = if asset1_amount > asset2_amount {
-                    (
-                        balance_changes[1].coin_type.to_string(),
-                        balance_changes[0].coin_type.to_string(),
-                    )
-                } else {
-                    (
-                        balance_changes[0].coin_type.to_string(),
-                        balance_changes[1].coin_type.to_string(),
-                    )
-                };
-
-                let _e = &event.parsed_json;
-                return Ok(TxType::Swap(Swap {
-                    pool: _e.get("pool_id").unwrap().as_str().unwrap().to_string(),
-                    dex: Dex::Bluefin,
-                    a2b: event.parsed_json.get("a2b").unwrap().as_bool().unwrap(),
-                    in_amount: _e
-                        .get("amount_in")
-                        .unwrap()
-                        .as_str()
-                        .unwrap()
-                        .parse::<i128>()
-                        .unwrap(),
-                    out_amount: _e
-                        .get("amount_out")
-                        .unwrap()
-                        .as_str()
-                        .unwrap()
-                        .parse::<i128>()?,
-                    in_token,
-                    out_token,
-                }));
-            } else {
-                println!("### txs: {}", serde_json::to_string(&tx_resp).unwrap());
-            }
-        }
-
-        println!("### txs: {}", serde_json::to_string(&tx_resp).unwrap());
-
-        bail!("Unknown tx type")
-    }
-
-    fn tx_gas(effect: SuiTransactionBlockEffects) -> BigDecimal {
-        let fee = effect.gas_cost_summary();
-        let fee = fee.computation_cost + fee.storage_cost - fee.storage_rebate;
-        BigDecimal::from(fee) / 10u128.pow(9)
-    }
-}
+use crate::transfer::TransferEvent;
 
 pub enum TxType {
     Transfer(TransferEvent),
@@ -235,7 +21,7 @@ pub struct Swap {
     pub out_token: String,
 }
 
-enum Dex {
+pub enum Dex {
     Cetus,
     Magma,
     Bluefin,
@@ -245,7 +31,7 @@ enum Dex {
 mod tests {
     use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-    use crate::Fetcher;
+    use super::fetcher::Fetcher;
 
     #[tokio::test]
     async fn test_log_sui_tx_resp() {
@@ -266,9 +52,6 @@ mod tests {
         .await
         .unwrap();
 
-        fetcher.fetch_txs().await.unwrap();
-
-        // let tx_resp = SuiTransactionBlockResponse::default();
-        // log_sui_tx_resp(tx_resp);
+        fetcher.fetch_txs(true).await.unwrap();
     }
 }
